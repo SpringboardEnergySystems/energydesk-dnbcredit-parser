a15f1191ba58270aad12d390ba17b9ee71451ba3
Steinar E <s.r.eiksen@gmail.com>
2022-11-05 12:59:55 +0100

    Fixing credit parser

--- dnbcreditparser/sample.py
+++ dnbcreditparser/sample.py
@@ -3,7 +3,9 @@ from dateutil import parser
 from os.path import join, dirname
 import  sys, environ, os
 from pathlib import Path
+from energydeskapi.sdk.common_utils import init_api
 from dnbcreditparser.parser import parse_credit_rating
+from dnbcreditparser.uploader import upload_ratings
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
                     handlers=[logging.FileHandler("pdfparser.log"),
@@ -12,6 +14,7 @@ logging.basicConfig(level=logging.INFO,
 
 if __name__ == '__main__':
     starter=dirname(__file__)
+    api_conn = init_api(dirname(__file__))
     from pathlib import Path
     for p in os.listdir("./data/"):
         print(p)
@@ -19,5 +22,8 @@ if __name__ == '__main__':
         filep = os.path.join(starter, path.absolute())
         #print(filep)
         dtiso, df=parse_credit_rating(filep)
+        if dtiso is None:
+            continue
         print(dtiso, df)
+        upload_ratings(api_conn, dtiso, df)
 
---
+++ dnbcreditparser/uploader.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+import json
+from energydeskapi.counterparts.counterparts_api import CounterPartsApi, CounterPartRating
+def upload_ratings(api_conn, isodt,df):
+    result = df.to_json(orient="records")
+    result=json.loads(result)
+    for rec in result:
+        cc=CounterPartRating( rec['company'], isodt, rec)
+        CounterPartsApi.upsert_credit_rating(api_conn, cc)
+
+    #print(result)
