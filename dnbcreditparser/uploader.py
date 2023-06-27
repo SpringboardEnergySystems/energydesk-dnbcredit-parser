f5e2c3446f346b4db444ba6f25dd370bb0a7866a
Steinar <s.r.eriksen@gmail.com>
2023-06-27 15:23:40 +0200

    tst

--- dnbcreditparser/uploader.py
+++ dnbcreditparser/uploader.py
@@ -4,7 +4,10 @@ import json
 from energydeskapi.counterparts.counterparts_api import CounterPartsApi, CounterPartRating
 def upload_ratings(api_conn, isodt,df):
     result = df.to_json(orient="records")
+    print(df)
+
     result=json.loads(result)
     for rec in result:
         cc=CounterPartRating( rec['company'], isodt, rec)
+
         CounterPartsApi.upsert_credit_rating(api_conn, cc)
